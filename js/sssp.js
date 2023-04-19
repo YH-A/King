f715d2a2b1fe407e3137b49ed0c408f965dc098d
YHA <48016809+YH-A@users.noreply.github.com>
2023-04-19 19:31:26 +0800

    Update sssp.js

--- js/sssp.js
+++ js/sssp.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/files\.(yuchenglw|honghufly)\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ghs/sssp.js
+^https:\/\/files\.(yuchenglw|honghufly)\.com url script-response-body https://raw.githubusercontent.com/YH-A/King/main/js/sssp.js
 
 [mitm]
 
